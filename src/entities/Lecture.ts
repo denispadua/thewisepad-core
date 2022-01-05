790ab990e115dc8570299070320ee754282ecc63
denispadua <denisdepadua@outlook.com>
2022-01-05 20:22:04 -0300

    feat: lectures have video url

--- src/entities/Lecture.ts
+++ src/entities/Lecture.ts
@@ -1,3 +1,10 @@
 export class Lecture{
+    readonly description: string
+    readonly videoUrl: string
+    // constructor(description: string, videoUrl: string){
+    //     this.description = description
+    //     this.videoUrl = videoUrl
+    // }
 
+     
 }
--- test/entities/course.spec.ts
+++ test/entities/course.spec.ts
@@ -4,17 +4,20 @@ describe('Course',()=>{
     it('should be able to add  lectures to modules', ()=>{
         const module = new Module('Fundamentals')
         const lecture: Lecture = {
-            description: 'Branching'
+            description: 'Branching',
+            videoUrl: 'http://youtube.com/1234'
         }
         module.add(lecture)
         expect(module.includes(lecture)).toBeTruthy()
     })
+    
 
     it('Should be able to add modules to courses', ()=>{
         const course = new Course('azure-devops', 'Continous Delivery and DevOps with Azure DevOps: Source control with git')
         const module = new Module('Fundamentals')
         const lecture: Lecture = {
-            description: 'Branching'
+            description: 'Branching',
+            videoUrl: 'http://youtube.com/1234'
         }
         module.add(lecture)
         course.add(module)
