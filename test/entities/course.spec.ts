7c6534c0edfd3203cc44b62d0a0f17cc4ba9ba0d
denispadua <denisdepadua@outlook.com>
2022-01-19 18:09:19 -0300

    feat: should handle exceeding position while rearranging

--- test/entities/course.spec.ts
+++ test/entities/course.spec.ts
@@ -11,20 +11,20 @@ describe('Course', () => {
     expect(course.includes(module)).toBeTruthy()
   })
 
-  it('should be able to rearrange the order of modules', ()=>{
-    const course = new Course('azure-devops','Continuous Delivery and DevOps with Azure DevOps: Source Control with Git')
+  it('should be able to rearrange the order of modules', () => {
+    const course = new Course('azure-devops', 'Continuous Delivery and DevOps with Azure DevOps: Source Control with Git')
     const fundamentalModule = new Module('Fundamentals')
     const branchingLecture: Lecture = new Lecture('Branching', 'https://youtube.com/1234')
     fundamentalModule.add(branchingLecture)
-    
+
     const courseOverviewModule = new Module('Course Overview')
     const courseOverviewLecture = new Lecture('Course Overview', 'https://youtube.com/789')
     courseOverviewModule.add(courseOverviewLecture)
-    
+
     const gitModule = new Module('Source control with Git on Azure DevOps')
     const introductionLecture = new Lecture('Introduction Lecture', 'http://youtube.com/abcdef')
     gitModule.add(introductionLecture)
-    
+
     course.add(fundamentalModule)
     course.add(courseOverviewModule)
     course.add(gitModule)
@@ -34,7 +34,30 @@ describe('Course', () => {
     expect(course.position(courseOverviewModule)).toBe(1)
     expect(course.position(fundamentalModule)).toBe(2)
     expect(course.position(gitModule)).toBe(3)
-
   })
 
+  it('should handle exceeding position while rearranging', () => {
+    const course = new Course('azure-devops', 'Continuous Delivery and DevOps with Azure DevOps: Source Control with Git')
+    const fundamentalModule = new Module('Fundamentals')
+    const branchingLecture: Lecture = new Lecture('Branching', 'https://youtube.com/1234')
+    fundamentalModule.add(branchingLecture)
+
+    const courseOverviewModule = new Module('Course Overview')
+    const courseOverviewLecture = new Lecture('Course Overview', 'https://youtube.com/789')
+    courseOverviewModule.add(courseOverviewLecture)
+
+    const gitModule = new Module('Source control with Git on Azure DevOps')
+    const introductionLecture = new Lecture('Introduction Lecture', 'http://youtube.com/abcdef')
+    gitModule.add(introductionLecture)
+
+    course.add(fundamentalModule)
+    course.add(courseOverviewModule)
+    course.add(gitModule)
+
+    course.move(fundamentalModule, 10)
+
+    expect(course.position(fundamentalModule)).toBe(1)
+    expect(course.position(courseOverviewModule)).toBe(2)
+    expect(course.position(gitModule)).toBe(3)
+  })
 })
