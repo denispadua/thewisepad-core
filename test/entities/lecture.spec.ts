e5c796185b49819d7efe5a71953307d4755241f8
denispadua <denisdepadua@outlook.com>
2022-01-24 22:31:34 -0300

    test: further material

---
+++ test/entities/lecture.spec.ts
@@ -0,0 +1,14 @@
+import { Lecture } from '@/entities'
+import { Material } from '@/entities/Material'
+import { Pdf } from '@/entities/Pdf'
+
+describe('Lecture', () => {
+  it('Should be able to add further material to lectures', () => {
+    const lecture: Lecture = new Lecture('Branching', 'http://youtube.com/1234')
+    const branchingPdf: Material = new Pdf('Branching', 'http://storage/branching.pdf')
+
+    lecture.add(branchingPdf)
+
+    expect(lecture.includes(branchingPdf)).toBeTruthy()
+  })
+})
