9360801912c6154cf5fec0fcdeea6c7560f2d9d8
denispadua <denisdepadua@outlook.com>
2022-02-11 22:23:09 -0300

    feat: handle position of unexisting material

--- src/entities/Lecture.ts
+++ src/entities/Lecture.ts
@@ -27,6 +27,10 @@ export class Lecture implements Part {
     return this.materials.remove(material)
   }
 
+  position(material: Material): Either<UnexistingElementError, number>{
+    return this.materials.position(material)
+  }
+  
   equals (other: Lecture): boolean {
     return this.description === other.description
   }
